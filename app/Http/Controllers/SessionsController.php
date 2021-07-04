c59df5c804a3c93fc66eee35c2b53ebed5a126a2
marstormad <marstormad@outlook.com>
2021-07-04 17:21:26 +0000

    7.4退出

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -35,6 +35,8 @@ class SessionsController extends Controller
 
     }
     function destroy(){
-
+        Auth::logout();
+        session()->flash('success','您已成功退出');
+        return redirect('login');
     }
 }
