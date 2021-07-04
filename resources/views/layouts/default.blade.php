b9de2acd6bd26647a9f8f88144dba3f790337571
marstormad <marstormad@outlook.com>
2021-07-04 00:53:10 +0000

    静态文件缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,12 +2,12 @@
 <html>
   <head>
     <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
-    <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <link rel="stylesheet" type="text/css" href="{{mix('css/app.css')}}">
   </head>
   <body>
   	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="/">Weibo App</a>
+        <a class="navbar-brand" href="/">Weibo Apps</a>
         <ul class="navbar-nav justify-content-end">
           <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
           <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
