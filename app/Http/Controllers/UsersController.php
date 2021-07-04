f6032058111547238243445f124abeb9194f42a1
marstormad <marstormad@outlook.com>
2021-07-04 11:30:35 +0000

    6.2用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,11 +1,16 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
 	public function create()
 	{
 		return view('users.create');
+	}
+	function show(User $user)
+	{
+		return view('users.show',compact('user'));
 	}
 }
