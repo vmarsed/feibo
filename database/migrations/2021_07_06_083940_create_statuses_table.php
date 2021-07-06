2242ffb72b4766611eaa97873377bd235c0072bc
marstormad <marstormad@outlook.com>
2021-07-06 08:44:46 +0000

    微博数据模型

---
+++ database/migrations/2021_07_06_083940_create_statuses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statuses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('content');
+            $table->integer('user_id')->index();//索引
+            $table->index(['created_at']);// 为 created_at 加索引
+            $table->timestamps(); // 这个产生 created_at 和 updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statuses');
+    }
+}
