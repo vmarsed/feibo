371453a18623675d59d61945277bde11873e084e
marstormad <marstormad@outlook.com>
2021-07-06 15:49:31 +0000

    10.6删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use App\Models\Status;
 
 class StatusesController extends Controller
 {
@@ -22,6 +23,17 @@ class StatusesController extends Controller
             'content'=>$request['content']
         ]);
         session()->flash('success','发布成功!');
+        return redirect()->back();
+    }
+    public function destroy(Status $status)
+    {
+        // blade 中使用 can 利用授权策略
+        // 控制器中使用 authroize() 方法,参数倒是一致
+        // 做删除授权的检测，不通过会抛出 403 异常。
+        $this->authorize('destroy', $status);
+        // 调用 Eloquent 模型的 delete 方法对该微博进行删除
+        $status->delete();
+        session()->flash('success', '微博已被成功删除！');
         return redirect()->back();
     }
 
--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -1,4 +1,3 @@
-    
 <li class="media mt-4 mb-4">
     <a href="{{ route('users.show', $user->id )}}">
         <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="mr-3 gravatar"/>
@@ -13,4 +12,12 @@
         </h5>
         {{ $status->content }}
     </div>
+
+    @can('destroy', $status)
+    <form action="{{ route('statuses.destroy', $status->id) }}" method="POST" onsubmit="return confirm('您确定要删除本条微博吗？');">
+        {{ csrf_field() }}
+        {{ method_field('DELETE') }}
+        <button type="submit" class="btn btn-sm btn-danger">删除</button>
+    </form>
+    @endcan
 </li>
